82fec6d6a00be04d008b0bf6e18f93a80af3d570
Rita Costa <rita.dev4u@gmail.com>
2025-09-13 13:16:11 +0100

    added a port for the server to listen on

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,14 @@
 import express from 'express'
 import { router } from './routes'
+
 const app = express()
+const PORT = 3000
+
 app.use(express.json())
 app.use(router)
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`)
+})
 
 export { app }
